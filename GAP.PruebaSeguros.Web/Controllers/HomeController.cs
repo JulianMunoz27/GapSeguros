16539123d4bf4feaaec8b947063ec3130fe8561f
Julian Andres Muñoz Bedoya <julian.munoz24@hotmail.com>
2019-10-12 16:48:26 -0500

    Project Creation

--- GAP.PruebaSeguros.Web/Controllers/HomeController.cs
+++ GAP.PruebaSeguros.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GAP.PruebaSeguros.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page - Julian";
 
             return View();
         }
